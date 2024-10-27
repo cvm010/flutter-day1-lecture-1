fb550fd976a81be7fe0bb363dd956f88e5959627
CVM <s778193047@gmail.com>
2024-10-27 11:54:51 +0530

    Day 1 lecture 1, Scaffold, gerneral architecture

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home : Scaffold(
+    appBar: AppBar(
+      title: Text('my first App'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('Hello ninjas'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+      },
+      child: Text('click'),
+    ),
+  ),
+
+));
