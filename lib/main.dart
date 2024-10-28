16b495cfabe4a9436011c5b0b80fe0d3c7281e87
CVM <s778193047@gmail.com>
2024-10-28 10:27:39 +0530

    Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home : Scaffold(
-    appBar: AppBar(
-      title: Text('my first App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-          'Hello ninjas',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.grey[600],
+  home : Home(),
+));
 
+
+
+
+// stateless widget is widget which does not change not with time
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('my first App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hello ninjas',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.grey[600],
+
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-      },
-      child: Text('click'),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
-
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        },
+        child: Text('click me'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
